a62a0ffbf0edaa6548147ffa94642187e34d256c
Vladislav2505 <tvlad554@gmail.com>
2023-08-24 23:58:19 +0300

    echo

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -11,7 +11,7 @@ class MainController extends Controller
     {
         $this->setMeta('Главная', 'Description', 'keywords');
         $names = $this->model->getNames();
-        echo 'Vlad';
+
         $this->setData(compact('names'));
     }
 }
